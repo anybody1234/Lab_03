734376ce530b413dd7e5c24b5e145faa9d0e5fc5
anybody1234 <167839131+anybody1234@users.noreply.github.com>
2024-11-26 18:40:27 +0700

    bai8

---
+++ OtherProjects/src/hust/soict/dsai/lab01/HelloWorld1.java
@@ -0,0 +1,7 @@
+package hust.soict.dsai.lab01 ;
+public class HelloWorld1{
+	public static void main(String args[]){
+		System.out.println("Xin chao \n cac ban!");
+		System.out.println("Hello \t world!");
+}
+}
